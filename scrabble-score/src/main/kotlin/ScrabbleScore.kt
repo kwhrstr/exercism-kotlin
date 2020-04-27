d522d81f02b780bdb7d2c2c8f1fc1a4107039c65
kawaharashotaro <kkawaharasshoutarou@gmail.com>
2020-04-27 23:59:09 +0900

    scrabble-score追加

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,6 +1,19 @@
 object ScrabbleScore {
-
+    /*
+    * go の場合は引数はruneを使うこと
+    * runeが何者かは調べてね
+    * 簡単にいうと2バイト文字に対しても文字単位で切り分けされたもの
+    * 2バイト文字はどんなものかというと日本語とか全角文字列のこと
+    * "aあiいu"を切り分けると[あ, a, い, u]
+    * だが適当にやると変な切り方になるがruneでやると
+    * 切り分けがうまくいく
+    * 昔の言語とかではここら辺が面倒だから日本語を嫌ったりする人もいる
+    * */
     fun scoreLetter(c: Char): Int {
+        /*
+         * 文字列が含まれているかのチェック
+         * go string contains runeとかで検索してね
+         * */
         if ("AEIOULNRST".contains(c)){
             return 1
         }
@@ -27,8 +40,17 @@ object ScrabbleScore {
 
     fun scoreWord(word: String): Int {
         var ans = 0
-        for (c in word.toUpperCase()) {
-            ans += scoreLetter(c)
+        /*
+        * loop処理
+        * go for loop stringとかで調べてね
+        * 文字列をloopならmaze-goでも使ってるから参考にしてね
+        * */
+        for (c in word) {
+            /*
+            * 文字列を大文字に変換している
+            * これはgo string toUpperとかで調べてね
+            * */
+            ans += scoreLetter(c.toUpperCase())
         }
         return ans
     }
