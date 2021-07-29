45d36c1d5d368fc8a3ef2412a708aeec5b55e841
kawaharashotaro <kkawaharasshoutarou@gmail.com>
2021-07-30 07:21:32 +0900

    char uppercase -> string upper case

--- raindrops/src/main/kotlin/Raindrops.kt
+++ raindrops/src/main/kotlin/Raindrops.kt
@@ -1,64 +1,16 @@
 object Raindrops {
-
     fun convert(n: Int): String {
-        var ans = ""
-        if (n % 3 == 0) {
-            ans += "Pling"
+        val conv = { i:Int -> when (i) {
+            3 -> "Pling"
+            5 -> "Plang"
+            7 -> "Plong"
+            else -> "" }
         }
-        if (n % 5 == 0) {
-            ans += "Plang"
-        }
-        if (n % 7 == 0) {
-            ans += "Plong"
-        }
-
-        /*
-        * 3, 5, 7で割り切れない場合をif文で書くなら
-        * if (n % 3 != 0 && n % 5 != 0 && n % 7 != 0) {}
-        * と演算子&&を使う
-        * ただしこの問題でこうかくのはあまり適切ではない
-        * nが11で割り切れる場合は "Pleng"を足すようにと言われたとき
-        * if (n % 11 != 0){}
-        * と
-        * if (n % 3 != 0 && n % 5 != 0 && n % 7 != 0　&& n % 11 != 0) {}
-        * を追加しないといけなくなるこのif文も複雑度が増していく
-        * 対して
-        * if(ans == "")はansの初期値を""と定義していて,ansの値が何も変更なかった
-        * ことを意味する => 今回はif文が全て素通りされたことを意味する => 3, 5, 7割り切れないことを
-        * 意味することになる
-        * この場合
-        * nが11で割り切れる場合は "Pleng"を足すようにと言われたときの条件を追加するとしても
-        * if (n % 11 != 0){}だけ追加するだけで良い
-        */
-        if (ans == ""){
-            /* Int -> Stringへ変換
-            * プログラミング言語は数値123
-            * と文字列"123"で別物
-            * golangではやり方が違うので調べること
-            * 検索例: go int string
-            */
-            ans = n.toString()
-        }
-        return ans
-    }
-}
-
-
-
-//Another solution
-fun convert(n: Int): String {
-    val conv = { i:Int -> when (i) {
-        3 -> "Pling"
-        5 -> "Plang"
-        7 -> "Plong"
-        else -> "" }
-    }
-    val song = sequenceOf(3, 5, 7)
+        val song = sequenceOf(3, 5, 7)
             .filter { n % it == 0 }
             .fold(""){acc, i -> acc + conv(i) }
-    return if (song.isEmpty()){
-        n.toString()
-    } else {
-        song
+        return song.ifEmpty {
+            n.toString()
+        }
     }
 }
--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,57 +1,18 @@
 object ScrabbleScore {
-    /*
-    * go の場合は引数はruneを使うこと
-    * runeが何者かは調べてね
-    * 簡単にいうと2バイト文字に対しても文字単位で切り分けされたもの
-    * 2バイト文字はどんなものかというと日本語とか全角文字列のこと
-    * "aあiいu"を切り分けると[あ, a, い, u]
-    * だが適当にやると変な切り方になるがruneでやると
-    * 切り分けがうまくいく
-    * 昔の言語とかではここら辺が面倒だから日本語を嫌ったりする人もいる
-    * */
     fun scoreLetter(c: Char): Int {
-        /*
-         * 文字列が含まれているかのチェック
-         * go string contains runeとかで検索してね
-         * */
-        if ("AEIOULNRST".contains(c)){
-            return 1
+        return when(c) {
+            in "AEIOULNRST" -> 1
+            in "DG" -> 2
+            in "BCMP" -> 3
+            in "FHVWY" -> 4
+            in "K" -> 5
+            in "JX" -> 8
+            in "QZ" -> 10
+            else -> 0
         }
-        if ("DG".contains(c)) {
-            return 2
-        }
-        if ("BCMP".contains(c)) {
-            return 3
-        }
-        if ("FHVWY".contains(c)) {
-            return 4
-        }
-        if ("K".contains(c)) {
-            return 5
-        }
-        if ("JX".contains(c)) {
-            return 8
-        }
-        if ("QZ".contains(c)) {
-            return 10
-        }
-        return 0
     }
 
     fun scoreWord(word: String): Int {
-        var ans = 0
-        /*
-        * loop処理
-        * go for loop stringとかで調べてね
-        * 文字列をloopならmaze-goでも使ってるから参考にしてね
-        * */
-        for (c in word.toUpperCase()) {
-            /*
-            * 文字列を大文字に変換している
-            * これはgo string toUpperとかで調べてね
-            * */
-            ans += scoreLetter(c)
-        }
-        return ans
+        return word.toUpperCase().fold(0){acc, c -> acc + scoreLetter(c) }
     }
 }
